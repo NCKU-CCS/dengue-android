e4c24cd34734f1eae7be6fa894e148b0d48bc9be
HsuTing <hsuting0106@gmail.com>
2016-03-08 14:47:43 +0800

    breeding sources submit: component

---
+++ app/src/main/java/com/example/hsuting/dengue_android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hsuting.dengue_android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hsuting on 16/3/8.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
+}
