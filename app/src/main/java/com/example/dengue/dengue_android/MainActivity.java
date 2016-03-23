3abfb2148b9f6fa19348b71973ae97cfb3281d62
HsuTing <hsuting0106@gmail.com>
2016-03-23 17:59:20 +0800

    add report list check

--- app/src/main/java/com/example/dengue/dengue_android/MainActivity.java
+++ app/src/main/java/com/example/dengue/dengue_android/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.dengue.dengue_android;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +21,8 @@ import android.widget.TextView;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -128,7 +133,7 @@ public class MainActivity extends AppCompatActivity implements
         Button menu_breedingSources = (Button)findViewById(R.id.menu_breedingSources);
         menu_breedingSources.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View w){
+            public void onClick(View w) {
                 setContentView(R.layout.breeding_sources_submit);
                 breedingSources_submit_submitButtonEvent();
             }
@@ -146,27 +151,96 @@ public class MainActivity extends AppCompatActivity implements
 
     // 'report list' button event
     private void reportList_buttonEvent() {
-        goBack(R.layout.menu_village_chief);
+        goBack(R.layout.menu_village_chief, new Runnable() {
+            @Override
+            public void run() {
+                menu_buttonsEvent();
+            }
+        });
         final String number = "10";
         final String[] name = new String[] {"地點1", "地點2", "地點3"};
         final String[] isDone = new String[] {"完成", "未查", "完成"};
         CharSequence[] Name = name;
         CharSequence[] IsDone = isDone;
 
-        TextView report_number = (TextView) findViewById(R.id.reportList_number);
-        report_number.setText("還有 " + number + " 個點待查");
+        TextView reportList_number = (TextView) findViewById(R.id.reportList_number);
+        reportList_number.setText("還有 " + number + " 個點待查");
 
-        ListView report_list = (ListView) findViewById(R.id.reportList_list);
-        report_list.setAdapter(new reportAdapter(this, Name, IsDone));
+        ListView reportList_list = (ListView) findViewById(R.id.reportList_list);
+        reportList_list.setAdapter(new reportAdapter(this, Name, IsDone));
+        reportList_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                setContentView(R.layout.report_list_check);
+                String date = "date";
+                String type = "type";
+                String description = "description";
+                reportList_checkButtonEvent(date, type, description);
+            }
+        });
+    }
+
+    // 'report list' check button event
+    private void reportList_checkButtonEvent(String date, String type, String description) {
+        goBack(R.layout.report_list, new Runnable() {
+            @Override
+            public void run() {
+                reportList_buttonEvent();
+            }
+        });
+
+        TextView reportList_check_date = (TextView) findViewById(R.id.reportList_check_date);
+        TextView reportList_check_type = (TextView) findViewById(R.id.reportList_check_type);
+        TextView reportList_check_description = (TextView) findViewById(R.id.reportList_check_description);
+        reportList_check_date.setText(reportList_check_date.getText().toString() + date);
+        reportList_check_type.setText(reportList_check_type.getText().toString() + type);
+        reportList_check_description.setText(reportList_check_description.getText().toString() + description);
+
+        final String[] ways = new String[] {"清除", "告知清理"};
+        Button reportList_check_yesButton = (Button) findViewById(R.id.reportList_check_yesButton);
+        reportList_check_yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle(R.string.reportList_check_dialogTitle)
+                        .setItems(ways, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                setContentView(R.layout.report_list);
+                                reportList_buttonEvent();
+                            }
+                        })
+                        .show();
+            }
+        });
+
+        Button reportList_check_noButton = (Button) findViewById(R.id.reportList_check_noButton);
+        reportList_check_noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.report_list);
+                reportList_buttonEvent();
+            }
+        });
     }
 
     // 'breeding sources submit' submit button event
     private void breedingSources_submit_submitButtonEvent() {
         if(isVillageChief) {
-            goBack(R.layout.menu_village_chief);
+            goBack(R.layout.menu_village_chief, new Runnable() {
+                @Override
+                public void run() {
+                    menu_buttonsEvent();
+                }
+            });
         }
         else {
-            goBack(R.layout.menu);
+            goBack(R.layout.menu, new Runnable() {
+                @Override
+                public void run() {
+                    menu_buttonsEvent();
+                }
+            });
         }
 
         Spinner breedingSources_submit_spinner = (Spinner)findViewById(R.id.breedingSources_submit_type_value);
@@ -192,18 +266,17 @@ public class MainActivity extends AppCompatActivity implements
                 data += "lon:" + lon + "\n";
                 data += "done: false";
                 output.setText(data);
-
             }
         });
     }
 
-    private void goBack(final int layoutID) {
+    private void goBack(final int layoutID, final Runnable buttonEvent) {
         ImageButton goBackButton = (ImageButton) findViewById(R.id.goback);
         goBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View w) {
                 setContentView(layoutID);
-                menu_buttonsEvent();
+                buttonEvent.run();
             }
         });
     }
