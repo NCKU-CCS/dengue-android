ce2ffcf9b2c8b9bd14a47ec66de1f1384b6c9b46
HsuTing <hsuting0106@gmail.com>
2016-04-20 19:29:19 +0800

    modify menu

--- app/src/main/java/com/example/dengue/dengue_android/hot.java
+++ app/src/main/java/com/example/dengue/dengue_android/hot.java
@@ -1,7 +1,23 @@
 package com.example.dengue.dengue_android;
 
-/**
- * Created by hsuting on 16/4/20.
- */
-public class hot {
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class hot extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.hot);
+        setWeb();
+        new menu(this);
+    }
+
+    private void setWeb() {
+        WebView web = (WebView) findViewById(R.id.hot_web);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.requestFocus();
+        web.loadUrl("http://real.taiwanstat.com/dengue-spatial-temporal/");
+    }
 }
--- app/src/main/java/com/example/dengue/dengue_android/menu.java
+++ app/src/main/java/com/example/dengue/dengue_android/menu.java
@@ -1,7 +1,46 @@
 package com.example.dengue.dengue_android;
 
-/**
- * Created by hsuting on 16/4/20.
- */
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
 public class menu {
+    private int [] Name = new int[]{
+        R.string.menu_reportList,
+                R.string.menu_bite,
+                R.string.menu_breedingSources,
+                R.string.menu_hot,
+                R.string.menu_hospital
+    };
+    private int [] Img = new int[]{
+        R.drawable.img,
+                R.drawable.img,
+                R.drawable.img,
+                R.drawable.img,
+                R.drawable.img
+    };
+
+    menu(Activity Main) {
+        GridView menu_list = (GridView) Main.findViewById(R.id.menu);
+        menu_list.setNumColumns(5);
+        menu_list.setAdapter(new menuAdapter(Main, Name, Img));
+        menu_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (Name[position]) {
+                    case R.string.menu_reportList:
+                        break;
+                    case R.string.menu_bite:
+                        break;
+                    case R.string.menu_breedingSources:
+                        break;
+                    case R.string.menu_hot:
+                        break;
+                    case R.string.menu_hospital:
+                        break;
+                }
+            }
+        });
+    }
 }
